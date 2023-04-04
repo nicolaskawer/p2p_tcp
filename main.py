a09ce40beed80e031985430f4a5a96db476681dc
nicolaskawer <nicolaskawer@gmail.com>
2023-04-04 19:36:32 +0300

    ver0.1

--- main.py
+++ main.py
@@ -4,7 +4,43 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import threading
 import socket
+servers_dict = {}
+clients_dict = {}
+my_data_protocol = []
 ports = [1000, 1001, 1002, 1003, 1004]
+def server_start(port_input):
+    # Create a socket and start listening on the port
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', port_input))
+    server_socket.listen()
+    def wait_for_conns():
+        # Start a new thread to wait for connections
+        while True:
+            conn, addr = server_socket.accept()
+            data_protocol = conn.recv(1)
+            type_, sub_type, len_, sub_len = data_protocol[0], data_protocol[1], data_protocol[2], data_protocol[3]
+            if type == 0:
+                servers_dict[addr[0]] = addr[1]
+                if sub_type == 0:
+                    for key, value in servers_dict.items():
+                        # send the key as a string
+                        server_socket.sendall(str(key).encode())
+
+                        # send a separator character
+                        server_socket.sendall('='.encode())
+
+                        # send the value as a string
+                        servers_dict.sendall(str(value).encode())
+
+                        # send a delimiter character
+                        servers_dict.sendall(';'.encode())
+            print(f'Connected to {addr}')
+            server_socket = {}
+
+
+
+
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,6 +49,12 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    while True:
+        port_index = int(input(f"Choose a port index between 0 and {len(ports) - 1}: "))
+        if port_index not in range(len(ports)):
+            print("Invalid port index")
+            exit(1)
+        port = ports[port_index]
+        server_start(port)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
